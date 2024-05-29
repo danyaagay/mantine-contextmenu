6b112378c6918e3df3e7a9e1c89a1259d40492e0
Danil <danniels.krouser@gmail.com>
2024-05-29 15:21:25 +0000

    Remove stuck interface

--- package/ContextMenuPortal.tsx
+++ package/ContextMenuPortal.tsx
@@ -1,5 +1,5 @@
 import { Portal } from '@mantine/core';
-import { useHotkeys, useWindowEvent } from '@mantine/hooks';
+import { useClickOutside, useHotkeys, useWindowEvent } from '@mantine/hooks';
 import { ContextMenu, type ContextMenuProps } from './ContextMenu';
 import { ContextMenuOverlay } from './ContextMenuOverlay';
 
@@ -7,16 +7,15 @@ export type ContextMenuPortalProps = ContextMenuProps & {
   zIndex?: number;
 };
 
-export function ContextMenuPortal({ onHide, zIndex, ...otherProps }: ContextMenuPortalProps) {
+export function ContextMenuPortal({ onHide, ...otherProps }: ContextMenuProps) {
   useWindowEvent('resize', onHide);
   useWindowEvent('scroll', onHide);
   useHotkeys([['Escape', onHide]]);
+  const ref = useClickOutside(onHide);
 
   return (
     <Portal>
-      <ContextMenuOverlay zIndex={zIndex} onHide={onHide}>
         <ContextMenu {...otherProps} onHide={onHide} />
-      </ContextMenuOverlay>
     </Portal>
   );
 }
